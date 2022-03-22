6dd2e5e6c3ed8499a3fdd72c425747266deae645
Leo Quigao <leo.quigao@thryv.com>
2022-03-22 21:59:05 +0800

    Removed unneeded imports

--- src/main/java/com/example/demo/RedirectController.java
+++ src/main/java/com/example/demo/RedirectController.java
@@ -3,8 +3,6 @@ package com.example.demo;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Mono;
 
 import java.net.URI;
 
